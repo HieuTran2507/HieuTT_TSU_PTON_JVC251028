e4d2e36a0bd30d5e1faf7ecde76388859ed48d37
hieutran <trantuanhieu2507@gmail.com>
2025-12-12 13:26:51 +0900

    12/12

--- session19/ex1/ex1.js
+++ session19/ex1/ex1.js
@@ -1,27 +1,73 @@
-let email = document.getElementById("email");
-let password = document.getElementById("password");
-let confirm_password = document.getElementById("confirm_password");
-let btn_register = document.querySelector(".btn_register");
+let users = JSON.parse(localStorage.getItem(`users`)) || [];
 
-btn_register.addEventListener("click", function(){
-    if(email.value === "" || password.value === "" || confirm_password.value === ""){
-        alert("vui lòng nhập đủ thông tin");
-        return;
-    }else{
-        localStorage.email = email.value;
-        while(password.value !== confirm_password.value){
-            alert("xác nhận mật khẩu không đúng vui lòng nhập lại");
-            password.value = "";
-            confirm_password.value = "";
-            return;
-        }
-        if(password.value === confirm_password.value){
-            localStorage.password = password.value;
-            localStorage.confirm_password = confirm_password.value;
-        }
+let form = document.getElementById(`form`);
+
+let errorEmail = document.querySelector(`.error-email`);
+let errorPassword = document.querySelector(`.error-password`);
+let errorConfirmPassword = document.querySelector(`.error-confirm-password`);
+
+form.addEventListener("submit",function(e){
+    e.preventDefault();
+    if(validateData(form)){
+        const newUser = {
+            id: Math.floor(Math.random()*10000),
+            email: form.email.value,
+            password: form.password.value,
+        };
+        users.push(newUser);
+        localStorage.setItem(`users`, JSON.stringify(users));
+        alert(`đăng ký thành công`);
+        form.email.value = ``;
+        form.password.value = ``;
+        form.confirmPassword.value = ``;
     }
-    alert("đăng ký thành công");
-    password.value = "";
-    email.value = "";
-    confirm_password.value = "";
 });
+
+
+function validateData(form){
+    let check = true;
+    // validate email
+    if(form.email.value === ``){
+        errorEmail.innerText = `Email trống`;
+        check = false;
+    } else if(!validEmail(form.email.value)){
+        errorEmail.innerText = `Email không hợp lệ`;
+        check = false;
+    } else {
+        errorEmail.innerText = ``;
+    }
+
+    // validate password 
+    if(form.password.value === ``){
+        errorPassword.innerText = `password trống`;
+        check = false;
+    } else if(!validPassword(form.password.value)){
+        errorPassword.innerText = `password không hợp lệ`;
+        check = false;
+    } else {
+        errorPassword.innerText = ``;
+    }
+
+    // validate confirm password
+    if(form.confirmPassword.value === ``){
+        errorConfirmPassword.innerText = `xác nhận mật khẩu trống`;
+        check = false;
+    } else if(form.password.value !== form.confirmPassword.value){
+        errorConfirmPassword.innerText = `Xác nhận mật khẩu không trùng khớp`;
+        check = false;
+    } else {
+        errorConfirmPassword.innerText = ``;
+    }
+
+    return check;
+}
+
+function validEmail(email){
+    return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+}
+
+function validPassword(password) {
+  return /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
+}
+
+console.log("js loaded");
